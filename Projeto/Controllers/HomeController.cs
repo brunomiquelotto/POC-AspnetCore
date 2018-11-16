0edba153055900b91b3eebcf9e1010b74535a73a
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-11-16 12:48:19 -0200

    Removido páginas desnecessárias

--- Projeto/Controllers/HomeController.cs
+++ Projeto/Controllers/HomeController.cs
@@ -8,28 +8,9 @@ using Projeto.Models;
 
 namespace Projeto.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
